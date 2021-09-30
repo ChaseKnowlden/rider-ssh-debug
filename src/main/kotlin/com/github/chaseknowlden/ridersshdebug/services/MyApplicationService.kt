d88da6fabb9d5edc014d819482a199f0e0beae17
GitHub Action <action@github.com>
2021-09-30 16:36:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/chaseknowlden/ridersshdebug/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.chaseknowlden.ridersshdebug.services
+
+import com.github.chaseknowlden.ridersshdebug.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
